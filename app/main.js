2d2093a97a245a5165ecd53a7a840cf910efbe00
JackF710 <jtfeery@gmail.com>
2024-11-14 13:58:52 -0500

    bbboi

--- app/main.js
+++ app/main.js
@@ -1,1 +1,26 @@
 import "./style.css";
+//get data
+//promises
+//show data
+
+async function getData() {
+  //return.promise
+  try {
+    const response = await fetch(
+      api - football - standings.azharimm.site / leagues
+    );
+    //guard clause
+    if (response.status != 200) {
+      throw new Error(response);
+    } else {
+      //convert promise to json
+      const data = await response.json();
+      console.log(data.data);
+      //unique to this API (valorant example)
+    }
+  } catch (error) {
+    alert("hey I could not find that agent");
+  }
+}
+
+getData();

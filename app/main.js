7e820beaebcfa3d1ac252ed4a63dd72aebe7900a
JackF710 <jtfeery@gmail.com>
2024-11-15 14:00:13 -0500

    BOI!

--- app/main.js
+++ app/main.js
@@ -1,25 +1,19 @@
 import "./style.css";
-//get data
-//promises
-//show data
 
 async function getData() {
-  //return.promise
   try {
     const response = await fetch(
-      [GET] - "4cacf8dfa0msh10883fde64ea546p13b9dfjsn202b29d5d5e0"
+      "https://api-football-standings.azharimm.site/leagues"
     );
-    //guard clause
     if (response.status != 200) {
       throw new Error(response);
     } else {
-      //convert promise to json
       const data = await response.json();
       console.log(data.data);
-      //unique to this API (valorant example)
+      data.data.forEach((league) => console.log(league.name));
     }
   } catch (error) {
-    alert("hey I could not find that agent");
+    alert("I could not find that boi!");
   }
 }
 
